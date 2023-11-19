e63d326cbc19526b5626dd19f1bf84cf0833bf09
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-19 16:15:07 +0300

    degiskenler

---
+++ degiskenler.php
@@ -0,0 +1,13 @@
+<?php
+$maasAli = 20000;
+$maasMehmet = 15000;
+$maasVolkan = 25000;
+
+$vergiOrani1 = 0.27;
+$vergiOrani2 = 0.35;
+
+echo $maasAli - ($maasAli * $vergiOrani1);  // Ali'nin Maaşı
+echo "<br/>";
+echo $maasMehmet - ($maasMehmet * $vergiOrani1);  // Mehmet'in Maaşı
+echo "<br/>";
+echo $maasVolkan - ($maasVolkan * $vergiOrani2);  // Volkan'ın Maaşı
