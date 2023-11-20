0037f721ce40788224d5ae9cba815e84c3d2b5f7
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-20 13:56:49 +0300

    diziler

---
+++ PhpVeriTipleri/08-diziler.php
@@ -0,0 +1,18 @@
+<?php
+
+// Diziler (Array)
+
+// 1- Numeric Arrays
+
+$renkler = array("Kırmızı", "Sarı", "Yeşil");
+
+echo gettype($renkler) . "<br>";
+echo $renkler[0] . "<br>";
+echo $renkler[1] . "<br>";
+echo $renkler[2] . "<br>";
+
+$sayilar = array(12, 42, 45, 7, 9, 89);
+
+echo $sayilar[0]."<br>";
+echo $sayilar[1]."<br>";
+echo $sayilar[2]."<br>";
