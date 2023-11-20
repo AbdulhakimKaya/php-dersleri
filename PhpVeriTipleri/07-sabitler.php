d7ef17ca030a9b7ed48377187f524ec3fb7a9094
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-20 13:02:06 +0300

    sabitler

---
+++ PhpVeriTipleri/07-sabitler.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    Php Sabitler:
+    Sabitler, veri saklamak için kullanılır ancak tanımlanan değer değiştirilemez.
+*/
+
+define("baglanti", "mysql bağlantı cümlesi");
+
+const baglanti2 = "mysql bağlantı cümlesi 2";
+const username = "AbdulhakimKaya";
+const password = "123456";
+
+
+
+echo baglanti."<br>";
+echo baglanti2."<br>";
+echo username."<br>";
+echo password."<br>";
+
