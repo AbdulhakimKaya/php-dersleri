d34cd8931c2ecef77683a461a32dc002a1e37676
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-28 15:34:33 +0300

    değer döndürme

---
+++ 05_Fonksiyonlar/02-deger-dondurme.php
@@ -0,0 +1,39 @@
+<?php
+
+function toplama () {
+    return 20 + 30;
+}
+
+echo toplama();
+
+echo "<br>";
+
+
+function sene() {
+    return date('Y');
+}
+
+function yasHesapla() {
+    return sene() - 2002;
+}
+
+$yas = yasHesapla();
+echo $yas;
+
+echo "<br>";
+
+
+function saat()
+{
+    return date('h');
+}
+
+function selamlama() {
+    if (saat() < 12) {
+        return "günaydın";
+    } else {
+        return "iyi günler";
+    }
+}
+
+echo selamlama();
