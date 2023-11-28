7d06578b1a9a6a3356ee31ec703ff497b45dcfd7
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-28 15:48:25 +0300

    parametre gönderme

---
+++ 05_Fonksiyonlar/03-parametre-gonderme.php
@@ -0,0 +1,43 @@
+<?php
+
+function selamlama($isim)
+{
+    echo "Merhaba " . $isim . "<br>";
+}
+
+selamlama("Mako");
+selamlama("Koko");
+
+
+function toplama($sayi1, $sayi2)
+{
+    echo $sayi1 + $sayi2;
+}
+
+toplama(2, 5);
+
+echo "<br>";
+
+
+function yasHesapla($yil)
+{
+    return date('Y') - $yil;
+}
+
+echo yasHesapla(2002);
+
+echo "<br>";
+
+
+function emekliligeKalanSure($dogumYili, $isim) {
+    $yas = yasHesapla($dogumYili);
+    $kalanSure = 65 - $yas;
+
+    if ($kalanSure > 0) {
+        return "$isim, emeklilige $kalanSure yıl kaldı";
+    } else {
+        return "$isim, zaten $kalanSure yıl önce emekli oldunuz";
+    }
+}
+
+echo emekliligeKalanSure(2002, "Abdulhakim");
