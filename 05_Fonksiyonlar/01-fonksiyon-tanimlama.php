1697f3d9a62006b046b06a7836aa8d9c0c5efcdd
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-11-28 15:25:38 +0300

    fonksiyon tanımlama

---
+++ 05_Fonksiyonlar/01-fonksiyon-tanimlama.php
@@ -0,0 +1,19 @@
+<?php
+
+function selamlama() {
+    for ($i = 0; $i < 5; $i++) {
+        echo "Merhaba Php" . "<br>";
+    }
+}
+
+selamlama();
+selamlama();
+
+
+function toplama() {
+    $a = 10;
+    $b = 20;
+    echo $a + $b;
+}
+
+toplama();
