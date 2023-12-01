bdd446f053a6e4e8d5c270bace351b74e1be2048
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-01 15:21:01 +0300

    partials views

---
+++ 06_Templates/partials/_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--  Bootstrap CSS  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"/>
+
+    <title>Title</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <a href="index.php">Anasayfa</a>
+        <a href="urunler.php">Ürünler</a>
+        <a href="contact.php">İletişim</a>
+    </nav>
+</header>
---
+++ 06_Templates/partials/_urunler.php
@@ -0,0 +1,5 @@
+<ul>
+    <li>Samsung S21</li>
+    <li>Samsung S22</li>
+    <li>Samsung S23</li>
+</ul>
