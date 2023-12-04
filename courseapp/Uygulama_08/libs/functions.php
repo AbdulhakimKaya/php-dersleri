dc41442cc3f1fc2af45facd96d192f0435ebf221
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-04 13:50:38 +0300

    uygulama-08

---
+++ courseapp/Uygulama_08/libs/functions.php
@@ -0,0 +1,32 @@
+<?php
+function kursEkle(&$kurslar, string $baslik, string $altBaslik, string $resim, string $yayinTarihi, int $begeniSayisi = 0, int $yorumSayisi = 0, bool $onay = true)
+{
+    $yeni_kurs[count($kurslar) + 1] = array(
+        "baslik" => $baslik,
+        "altBaslik" => $altBaslik,
+        "resim" => $resim,
+        "yayinTarihi" => $yayinTarihi,
+        "begeniSayisi" => $begeniSayisi,
+        "yorumSayisi" => $yorumSayisi,
+        "onay" => $onay,
+    );
+
+    $kurslar = array_merge($kurslar, $yeni_kurs);
+}
+
+function urlDuzenle($baslik)
+{
+    return str_replace([" ", "@", "."], ["-", "", "-"], strtolower($baslik));       // baslikdaki bazı karakterler istenilen karakterler ile değiştirilir
+}
+
+
+function kisaAciklama($altBaslik)
+{
+    if (strlen($altBaslik) > 50) {
+        return substr($altBaslik, 0, 50) . "...";
+    } else {
+        return $altBaslik;
+    }
+}
+
+?>
