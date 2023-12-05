511d731ef5c0aa195ec10cc8f5560a18e9b70f1f
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-05 16:45:21 +0300

    dosya özelliklerinin kontrolü

--- 08_DosyaYukleme/dosya-yukleme.php
+++ 08_DosyaYukleme/dosya-yukleme.php
@@ -1,3 +1,52 @@
+<?php
+
+// Dosya yükleme
+
+if (isset($_POST['btnFileUpload']) && $_POST['btnFileUpload'] == "Upload") {
+    echo "<pre>";
+    print_r($_FILES['fileToUpload']);
+    print_r($_POST);
+    echo "</pre>";
+
+    // Dosyanın klasöre kaydedilmesi
+
+    // Dosya özelliklerinin kontrolü
+
+    $uploadOk = true;
+
+    $dest_path = "./uploadedFiles/";
+
+    $filename = $_FILES["fileToUpload"]["name"];
+    $fileSize = $_FILES["fileToUpload"]["size"];
+
+    if (empty($filename)) {
+        $uploadOk = false;
+        echo "dosya seçiniz" . "<br>";
+    }
+
+    if ($fileSize > 100000) {
+        $uploadOk = false;
+        echo "Dosya boyutu fazla" . "<br>";
+    }
+
+    $fileSourcePath = $_FILES["fileToUpload"]["tmp_name"];
+
+    $fileDestPath = $dest_path . $filename;
+
+    if (!$uploadOk) {
+        echo "dosya yüklenmedi" . "<br>";
+    } else {
+        if (move_uploaded_file($fileSourcePath, $fileDestPath)) {
+            echo "dosya yüklendi" . "<br>";
+        } else {
+            echo "hata" . "<br>";
+        }
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +64,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <form method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="username">Kullanıcı Adı</label>
                     <input type="text" name="username" class="form-control">
