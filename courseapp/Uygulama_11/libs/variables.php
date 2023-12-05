6e075c7dccecf13f16f11a5eb12fdbc79bd9b59b
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-05 16:00:32 +0300

    uygulama-11

---
+++ courseapp/Uygulama_11/libs/variables.php
@@ -0,0 +1,68 @@
+<?php
+
+const title = "Popüler Kurslar";
+
+$kategoriler = array(
+    array(
+        "kategori_adi" => "Programlama",
+        "aktif" => true,
+    ),
+    array(
+        "kategori_adi" => "Web Geliştirme",
+        "aktif" => false,
+    ),
+    array(
+        "kategori_adi" => "Veri Analizi",
+        "aktif" => false,
+    ),
+    array(
+        "kategori_adi" => "Ofis Uygulamaları",
+        "aktif" => false,
+    ),
+);
+
+$kurslar = array(
+    "1" => array(
+        "baslik" => "Php Kursu",
+        "altBaslik" => "Sıfırdan ileri seviye Php ile web geliştirme",
+        "resim" => "1.jpg",
+        "yayinTarihi" => "01.01.2032",
+        "begeniSayisi" => 0,
+        "yorumSayisi" => 10,
+        "onay" => true,
+    ),
+    "2" => array(
+        "baslik" => "Python Kursu",
+        "altBaslik" => "Sıfırdan ileri seviye Python prrogramlama",
+        "resim" => "2.jpg",
+        "yayinTarihi" => "03.03.2032",
+        "begeniSayisi" => 10,
+        "yorumSayisi" => 0,
+        "onay" => true,
+    ),
+    "3" => array(
+        "baslik" => "Node.js Kursu",
+        "altBaslik" => "Sıfırdan ileri seviye Node.js ile web geliştirme",
+        "resim" => "3.jpg",
+        "yayinTarihi" => "05.05.2032",
+        "begeniSayisi" => 10,
+        "yorumSayisi" => 20,
+        "onay" => true,
+    ),
+);
+
+$sehirler = array(
+    06 => "Adana",
+    21 => "Diyarbakır",
+    23 => "Elazığ",
+    34 => "İstanbul",
+    35 => "İzmir"
+);
+
+$hobiler = array(
+    1 => "Sinema",
+    2 => "Fotoğrafçılık",
+    3 => "Müzik",
+);
+
+?>
---
+++ courseapp/Uygulama_11/register.php
@@ -0,0 +1,127 @@
+<?php
+
+require "libs/variables.php";
+require "libs/functions.php";
+
+?>
+
+
+<?php include 'partials/_header.php' ?>
+
+<?php include 'partials/_navbar.php' ?>
+
+<?php
+
+$usernameErr = $emailErr = $passwordErr = $repasswordErr = $cityErr = $hobbiesErr = "";
+$username = $email = $password = $repassword = $city = "";
+$hobbies = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty($_POST["username"])) {
+        $usernameErr = "username gerekli alan" . "<br>";
+    } else {
+        $username = safe_html($_POST["username"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "email gerekli alan" . "<br>";
+    } else {
+        $email = safe_html($_POST["email"]);
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "password gerekli alan" . "<br>";
+    } else {
+        $password = safe_html($_POST["password"]);
+    }
+
+    if (empty($_POST["repassword"])) {
+        $repasswordErr = "repassword gerekli alan" . "<br>";
+    } else {
+        $repassword = safe_html($_POST["repassword"]);
+    }
+
+    if (($_POST["password"]) != ($_POST["repassword"])) {
+        $repasswordErr = "parola tekrarı alanı eşleşmiyor" . "<br>";
+    } else {
+        $repassword = safe_html($_POST["repassword"]);
+    }
+
+    if (($_POST["city"]) == -1) {
+        $cityErr = "city seçilmeli" . "<br>";
+    } else {
+        $city = $_POST["city"];
+    }
+
+    if (!isset($_POST["hobbies"])) {
+        $hobbiesErr = "hobiler seçilmeli" . "<br>";
+    } else {
+        $hobbies = $_POST["hobbies"];
+    }
+}
+
+?>
+
+<div class="container my-3">
+    <div class="row">
+        <div class="col-12">
+            <form action="register.php" method="POST">
+                <div class="mb-3">
+                    <label for="username">Kullanıcı adı</label>
+                    <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $usernameErr; ?></div>
+                </div>
+                <div class="mb-3">
+                    <label for="email">Eposta</label>
+                    <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $emailErr; ?></div>
+                </div>
+                <div class="mb-3">
+                    <label for="password">Parola</label>
+                    <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $passwordErr; ?></div>
+                </div>
+                <div class="mb-3">
+                    <label for="repassword">Parola Tekrarı</label>
+                    <input type="password" name="repassword" class="form-control" value="<?php echo $repassword; ?>">
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $repasswordErr; ?></div>
+                </div>
+                <div class="mb-3">
+                    <label for="city">İl</label>
+                    <select name="city" class="form-select">
+                        <option value="-1" selected>İl seçiniz..</option>
+                        <?php foreach ($sehirler as $plaka => $sehir): ?>
+                            <option
+                                    value="<?php echo $plaka; ?>"
+                                <?php echo $city == $plaka ? ' selected' : ''; ?>
+                            >
+                                <?php echo $sehir; ?>
+                            </option>
+                        <?php endforeach ?>
+                    </select>
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $cityErr; ?></div>
+                </div>
+                <div class="mb-3">
+                    <label for="hobbies">Hobiler</label>
+                    <?php foreach ($hobiler as $id => $hobi): ?>
+                        <div class="form-check">
+                            <input
+                                    type="checkbox"
+                                    name="hobbies[]"
+                                    value="<?php echo $hobi ?>"
+                                    id="hobbies_<?php echo $id ?>"
+                                <?php if (in_array($hobi, $hobbies)) echo 'checked' ?>
+                            >
+                            <label for="hobbies_<?php echo $id ?>" class="form-check-label"><?php echo $hobi ?></label>
+                        </div>
+                    <?php endforeach ?>
+                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $hobbiesErr; ?></div>
+                </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Kayıt Ol</button>
+        </form>
+    </div>
+</div>
+
+<?php include 'partials/_footer.php' ?>
