2ef78e7ac3eaeaa2d3cf5bed5cefc24b30032b8b
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-05 16:33:26 +0300

    dosyanın klasöre kaydedilmesi

--- 08_DosyaYukleme/upload.php
+++ 08_DosyaYukleme/upload.php
@@ -1,8 +1,25 @@
 <?php
 
+// Dosya yükleme
+
 if (isset($_POST['btnFileUpload']) && $_POST['btnFileUpload'] == "Upload") {
     echo "<pre>";
     print_r($_FILES['fileToUpload']);
     print_r($_POST);
     echo "</pre>";
+
+    // Dosyanın klasöre kaydedilmesi
+    $dest_path = "./uploadedFiles/";
+
+    $filename = $_FILES["fileToUpload"]["name"];
+    $fileSourcePath = $_FILES["fileToUpload"]["tmp_name"];
+
+    $fileDestPath = $dest_path.$filename;
+
+    if (move_uploaded_file($fileSourcePath, $fileDestPath)) {
+        echo "dosya yüklendi";
+    } else {
+        echo "hata";
+    }
+
 }
