59e2b1efd66c0ba4c4c2a934f07423a1c2516939
Abdulhakim KAYA <abdulhakimkaya2199@gmail.com>
2023-12-10 13:41:24 +0300

    uygulama-15 admin kurs ekleme sayfasında kategoriler alanın kaldırılması

--- courseapp/Uygulama_15/course-create.php
+++ courseapp/Uygulama_15/course-create.php
@@ -17,8 +17,6 @@ session_start();
 $baslik = $baslikErr = "";
 $altBaslik = $altBaslikErr = "";
 $resim = $resimErr = "";
-$category = "0";
-$categoryErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -41,14 +39,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $resim = $_FILES["imageFile"]["name"];
     }
 
-    if ($_POST["category"] == "0") {
-        $categoryErr = "kategori seçmelisiniz";
-    } else {
-        $category = $_POST["category"];
-    }
-
-    if (empty($baslikErr) && empty($altBaslikErr) && empty($resimErr) && empty($categoryErr)) {
-        createCourse($baslik, $altBaslik, $resim, $category);
+    if (empty($baslikErr) && empty($altBaslikErr) && empty($resimErr)) {
+        createCourse($baslik, $altBaslik, $resim);
         $_SESSION["message"] = $baslik . " isimli kurs eklendi";
         $_SESSION["type"] = "success";
         header('Location: admin-courses.php');
@@ -79,19 +71,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </div>
                         <div class="badge text-bg-danger p-2 mt-2"><?php echo $resimErr; ?></div>
                     </div>
-                    <div class="mb-3">
-                        <label for="category">Kategori</label>
-                        <select name="category" id="category" class="form-select">
-                            <option value="0" selected>Seçiniz..</option>
-                            <?php foreach (getCategories() as $c): ?>
-                                <option value="<?php echo $c["id"] ?>"><?php echo $c["kategori_adi"] ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <div class="badge text-bg-danger p-2 mt-2"><?php echo $categoryErr; ?></div>
-                    <script type="text/javascript">
-                        document.getElementById("category").value = "<?php echo $category; ?>"
-                    </script>
 
                     <button type="submit" class="btn btn-primary">Kaydet</button>
                 </form>
